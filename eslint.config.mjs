bd1b905f5b846e158525cb4726ae3890e33969b4
Mika Jeske <Jeskem@students.uni-marburg.de>
2026-02-14 17:27:00 +0100

    build: fixed issues with the 'workflow-editor' project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,8 @@ export default defineConfig([
             parser: tseslint.parser,
 
             parserOptions: {
-                project: ['tsconfig.json'],
+                project: ['tsconfig.eslint.json'],
+                tsconfigRootDir: import.meta.dirname,
                 createDefaultProgram: false,
             },
         },
